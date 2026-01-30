3482321b190bb0bff46ae7f59b04848e8f24bd5b
bibletrccc <spacedna@hotmail.com>
2026-01-30 18:43:24 -0500

    call myip.free.nf

---
+++ api/getmyip.php
@@ -0,0 +1,13 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+//call https://myip.free.nf/ to refresh it every 30 days
+$data = file_get_contents('https://myip.free.nf/');
+header('Content-Type: text/html; charset=UTF-8');
+//echo $data;
+echo 'Call... https://myip.free.nf/'
+?>
